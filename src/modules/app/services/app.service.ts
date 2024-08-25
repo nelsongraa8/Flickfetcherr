45bae0ee6142c9993c1427317fc763a789e4b6a5
Nelson R. Graña <nelsongraa8@gmail.com>
2024-08-25 18:00:07 +0200

    feat(http): crea service para traer los datos

--- src/modules/app/services/app.service.ts
+++ src/modules/app/services/app.service.ts
@@ -1,10 +1,23 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): any {
-    return {
-      string: 'Hello World!',
-    };
+
+  constructor(private httpService: HttpService) {}
+
+  public async getHello(): Promise<any> {
+
+    return this.responseHttpExternalService().then((response) => {
+      return response.data;
+    }
+    ).catch((error) => {
+      return error;
+    });
+  }
+
+  private async responseHttpExternalService(): Promise<any> {
+    return await firstValueFrom(this.httpService.get('http://192.168.1.2:9117/api/v2.0/indexers/dontorrent/results/torznab/api?apikey=2qcu5p593jafl80xxda0ssni49ueizcm&t=search&cat=&q=Garfield'));
   }
 }
