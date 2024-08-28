5fe271f1dba474fc75356e842860ed8b9f0b1387
Nelson R. Graña <nelsongraa8@gmail.com>
2024-08-28 09:13:33 +0200

    feat: cambia la forma en la que se consulta

--- src/modules/app/services/app.service.ts
+++ src/modules/app/services/app.service.ts
@@ -5,23 +5,47 @@ import { parseStringPromise } from 'xml2js';
 
 @Injectable()
 export class AppService {
+  movieName: string;
 
   constructor(private httpService: HttpService) {}
 
-  public async getHello(): Promise<any> {
-    return this.responseHttpExternalService().then((response) => {
-      return this.transformTextToJson(response.data);
-    }
-    ).catch((error) => {
-      return error;
-    });
+  public async getHello(movieName: string): Promise<any> {
+    this.movieName = movieName;
+
+    return this.responseHttpExternalService()
+      .then(response => {
+        return this.transformTextToJson(response.data);
+      })
+      .catch(error => {
+        return error;
+      });
   }
 
   private async responseHttpExternalService(): Promise<any> {
-    return await firstValueFrom(this.httpService.get('http://192.168.1.2:9117/api/v2.0/indexers/dontorrent/results/torznab/api?apikey=2qcu5p593jafl80xxda0ssni49ueizcm&t=search&cat=&q=Garfield'));
+    return await firstValueFrom(
+      this.httpService.get(this.generateURlJackett()),
+    );
   }
 
   private transformTextToJson(textToJson: any): any {
     return parseStringPromise(textToJson);
+
+  private generateURlJackett(): string {
+    const jackett = {
+      protocol: 'http',
+      host: '192.168.1.2',
+      port: '9117',
+      apiKey: '2qcu5p593jafl80xxda0ssni49ueizcm',
+  };
+
+  const baseUrl = `${jackett.protocol}://${jackett.host}:${jackett.port}/api/v2.0/indexers/dontorrent/results/torznab/api`;
+  const params = new URLSearchParams({
+      apikey: jackett.apiKey,
+      t: 'search',
+      cat: '',
+      q: this.movieName
+  });
+
+  return `${baseUrl}?${params.toString()}`;
   }
 }
