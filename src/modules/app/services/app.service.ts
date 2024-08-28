0871defeb9d45a14bc758164f5a2c4d35de3a1fd
Nelson R. Graña <nelsongraa8@gmail.com>
2024-08-29 00:17:24 +0200

    feat: cambia url de peticion a jackett

--- src/modules/app/services/app.service.ts
+++ src/modules/app/services/app.service.ts
@@ -1,7 +1,6 @@
 
 import { Injectable } from '@nestjs/common';
-import { HttpExternalService } from 'src/core/service/http-external.service';
-import { parseStringPromise } from 'xml2js';
+import { HttpExternalService } from '@core/service/http-external.service';
 
 @Injectable()
 export class AppService {
@@ -18,18 +17,17 @@ export class AppService {
   private generateURlJackett(): string {
     const jackett = {
       protocol: 'http',
-      host: '192.168.1.2',
+      ipDomain: '192.168.1.2',
       port: '9117',
       apiKey: '2qcu5p593jafl80xxda0ssni49ueizcm',
       indexer: 'all'
   };
 
-  const baseUrl = `${jackett.protocol}://${jackett.host}:${jackett.port}/api/v2.0/indexers/${jackett.indexer}/results/torznab/api`;
+  const host = `${jackett.protocol}://${jackett.ipDomain}:${jackett.port}`
+  const baseUrl = `${host}/api/v2.0/indexers/${jackett.indexer}/results`;
   const params = new URLSearchParams({
       apikey: jackett.apiKey,
-      t: 'search',
-      cat: '',
-      q: this.movieName
+      Query: this.movieName,
   });
 
   return `${baseUrl}?${params.toString()}`;
