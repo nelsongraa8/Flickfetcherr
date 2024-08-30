9037d3b70e27d8cf34d8e8b33054578929d8fb39
Nelson R. Graña <nelsongraa8@gmail.com>
2024-08-30 14:23:14 +0200

    test: crea nuevo test para este nuevo controlador

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '@modules/app/app.module';
+import { MovieNowModule } from '@modules/movie-now/movie-now.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+        imports: [AppModule, MovieNowModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -27,6 +28,21 @@ describe('AppController (e2e)', () => {
         
         expect(body).toHaveProperty('Indexers');
         expect(Array.isArray(body.Indexers)).toBe(true);
+      });
+  });
+
+  it('movienow (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/movienow')
+      .expect(200)
+      .expect((res) => {
+        const body = res.body;
+
+        expect(body).toHaveProperty('page');
+        expect(body.page).toBe(1);
+        
+        expect(body).toHaveProperty('results');
+        expect(Array.isArray(body.results)).toBe(true);
       });
   });
 });
