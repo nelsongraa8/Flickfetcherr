14f2e14fea32479ca25a7512f92f06786e254371
Nelson R. Graña <nelsongraa8@gmail.com>
2024-08-30 14:24:17 +0200

    chore: elimina una importación innecesaria

--- src/modules/app/controllers/app.controller.ts
+++ src/modules/app/controllers/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from '@modules/app/services/app.service';
-import { Observable } from 'rxjs';
 
 @Controller('search')
 export class AppController {
