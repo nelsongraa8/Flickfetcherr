4f6b3d956712347df17f1b8e383958efb26bcb60
Nelson R. Graña <nelsongraa8@gmail.com>
2024-08-30 10:12:50 +0200

    feat: importar el shred module en el app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from '@modules/app/controllers/app.controller';
 import { AppService } from '@modules/app/services/app.service';
 import { HttpModule } from '@nestjs/axios';
 import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
 
 @Module({
-  imports: [HttpModule, CoreModule],
+  imports: [HttpModule, CoreModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
